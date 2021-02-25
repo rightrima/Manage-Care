50973014bc7604ad968a1f14a7e42c05fa451e3c
rightrima <32514796+rightrima@users.noreply.github.com>
2021-02-25 13:09:15 +0530

    a comment got added

--- result.js
+++ result.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+//Test for react.js
 const Result = ({score, playAgain}) => (
   <div className="score-board">
     <div className="score">You scored {score} / 5 correct answers!</div>
